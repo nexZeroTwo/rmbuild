4bc1694695e970a4d4ee532b266799b54bd7a57b
Andrew "Akari" Alexeyew <akari@dbc.1gb.ua>
2016-04-28 22:34:50 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='rmbuild',
+    version="0.1",
+    description='RocketMinsta build system',
+    url='http://github.com/nexAkari/rmbuild',
+    author='Andrew "Akari" Alexeyew"',
+    author_email='akari@alienslab.net',
+    license='WTFPL',
+    packages=['rmbuild'],
+)
